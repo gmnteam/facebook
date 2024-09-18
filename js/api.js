f45bbdc26a9b3b94c962706954855fdd727b1b51
Gmn Team <atmin.gmn@gmail.com>
2024-09-19 06:56:41 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-export const api = 'https://fbdl-weld.vercel.app/video-info/?url=';
+export const api = 'https://fbdl-weld.vercel.app/video-info?url=';
 export function uri(url) {
   return encodeURIComponent(url);
 }
